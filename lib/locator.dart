b2cd692a3f5d063e12fd21c6ad070c1c29960e43
Luis Arismendi <larismendi@teravisiontech.com>
2020-08-22 17:01:54 -0500

    fix

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:myapp/src/app.dart';
-import 'package:myapp/src/services/auth_service.dart';
-import 'package:myapp/src/services/auth_implementation_service.dart';
+// import 'package:myapp/src/services/auth_service.dart';
+// import 'package:myapp/src/services/auth_implementation_service.dart';
 import 'package:myapp/src/services/repository_service.dart';
 import 'package:myapp/src/services/repository_implementation_service.dart';
 
@@ -10,6 +10,6 @@ GetIt locator = new GetIt();
 void main() {
   locator.registerSingleton<RepositoryService>(
       new RepositoryImplementationService());
-  locator.registerSingleton<AuthService>(new AuthImplementationService());
+  // locator.registerSingleton<AuthService>(new AuthImplementationService());
   runApp(App());
 }
